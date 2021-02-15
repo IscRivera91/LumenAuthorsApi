c35589f2262253649570813e76d554f8383d6e5d
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-02-15 16:13:57 -0600

    cambios al seeder para crear 50 authors

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
+        Author::factory(50)->create();
     }
 }
