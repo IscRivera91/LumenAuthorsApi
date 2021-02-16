dd9fb73fd88185ca774eae5fbf5b3180be08fe79
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-02-15 19:34:11 -0600

    retonor de exception with errorResponse trait

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,11 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponse;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -11,6 +14,7 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponse;
     /**
      * A list of the exception types that should not be reported.
      *
@@ -49,6 +53,40 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if ($exception instanceof HttpException) {
+            $code = $exception->getStatusCode();
+            $message = Response::$statusTexts[$code];
+
+            return $this->errorResponse($message, $code);
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+
+            $modelo = strtolower(class_basename($exception->getModel()));
+
+            return $this->errorResponse("No existe una instancia del modelo {$modelo} con el id dado", Response::HTTP_NOT_FOUND);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_FORBIDDEN);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($exception instanceof ValidationException) {
+            $errores = $exception->validator->errors()->getMessages();
+
+            return $this->errorResponse($errores, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if (env('APP_DEBUG', false)) {
+            return parent::render($request, $exception);
+        }
+        
+        return $this->errorResponse('Error inesperado. intente mas tarde',Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
